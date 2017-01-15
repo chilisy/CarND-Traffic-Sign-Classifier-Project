ceedfa8c01ecbb872738c76b29ff48d7e58b9394
liheng <liheng.lc.chen@gmail.com>
2017-01-15 20:10:20 +0100

    chili: start export functions to code-folder

---
+++ code/main.py
@@ -0,0 +1,11 @@
+import pickle, os
+import numpy as np
+import pandas
+import zipfile
+from IPython.core.debugger import Tracer
+import matplotlib.pyplot as plt
+import random
+from sklearn.utils import shuffle
+
+
+
---
+++ code/nn_functions.py
@@ -0,0 +1,5 @@
+import numpy as np
+import random
+from sklearn.utils import shuffle
+
+import tensorflow as tf
