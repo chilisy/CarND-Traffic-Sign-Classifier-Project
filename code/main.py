d446c1e05f9f905821defdbe6e25390706c1cc9c
liheng <liheng.lc.chen@gmail.com>
2017-01-15 22:45:38 +0100

    chili: LeNet and optimizer

--- code/main.py
+++ code/main.py
@@ -1,15 +1,16 @@
 import pickle, os
 import numpy as np
-import pandas
+from nn_functions import *
 import zipfile
 import matplotlib.pyplot as plt
 import random
 from sklearn.utils import shuffle
 
 cwd = os.getcwd()
-data_dir = '../traffic-signs-data'
+data_dir = '/../traffic-signs-data'
 training_zipfile = cwd + data_dir + '/train.zip'
 
+# unzip train.zip if pickle-file not exist
 if 'train.p' not in os.listdir(cwd + data_dir):
     with zipfile.ZipFile(training_zipfile) as z:
         z.extract('train.p', cwd + data_dir)
@@ -22,9 +23,25 @@ with open(training_file, mode='rb') as f:
 with open(testing_file, mode='rb') as f:
     test = pickle.load(f)
 
-X_train, y_train = train['features'], train['labels']
+X_train_raw, y_train = train['features'], train['labels']
 X_test, y_test = test['features'], test['labels']
 X_sizes = train['sizes']
 X_coords = train['coords']
+
+# randomize data set
+X_train, X_sizes, X_coords, y_train = shuffle(X_train_raw, X_sizes, X_coords, y_train)
+
+# Plot
+showPlot = False
+if showPlot:
+    # Get random sign
+    index = random.randint(0, len(X_train))
+    image = X_train[index].squeeze()
+
+    plt.figure()
+    plt.imshow(image)
+    plt.show()
+    print(get_signname_from_index(y_train[index]))
+
 
 
--- code/nn_functions.py
+++ code/nn_functions.py
@@ -1,5 +1,109 @@
 import numpy as np
 import random
-from sklearn.utils import shuffle
+from tensorflow.contrib.layers import flatten
+import pandas
 
 import tensorflow as tf
+
+
+def get_signname_from_index(index):
+    csv_file = '../signnames.csv'
+    col_names = ['ClassId', 'SignName']
+    data = pandas.read_csv(csv_file, names=col_names)
+    signnames = data.SignName.tolist()
+    signnames = signnames[1:len(signnames)]
+
+    return signnames[index]
+
+
+def conv2d(inputs, weights, bias, stride=2, padding='VALID'):
+    strides = [1, stride, stride, 1]
+    conv = tf.nn.conv2d(inputs, weights, strides, padding)
+    out = tf.nn.bias_add(conv, bias)
+    return out
+
+
+def activation(features, opt='relu'):
+    if opt == 'relu':
+        return tf.nn.relu(features)
+    elif opt == 'sigmoid':
+        return tf.nn.sigmoid(features)
+    elif opt == 'tanh':
+        return tf.nn.tanh(features)
+
+
+def maxpool2d(inputs, ksize, stride, padding='VALID'):
+    ksizes = [1, ksize, ksize, 1]
+    strides = [1, stride, stride, 1]
+
+    out = tf.nn.max_pool(inputs, ksizes, strides, padding)
+    return out
+
+
+def LeNet(x):
+    # Arguments used for tf.truncated_normal, randomly defines variables for the weights and biases for each layer
+    mu = 0
+    sigma = 0.1
+
+    # Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
+    layer1_weights = tf.Variable(tf.truncated_normal(shape=(5, 5, 3, 6), mean=mu, stddev=sigma))
+    layer1_bias = tf.Variable(tf.zeros(6))
+    layer1 = conv2d(x, layer1_weights, layer1_bias, stride=1)
+
+    # Activation.
+    layer1 = activation(layer1, opt='relu')
+
+    # Pooling. Input = 28x28x6. Output = 14x14x6.
+    pool1 = maxpool2d(layer1, 2, 2)
+
+    # Layer 2: Convolutional. Output = 10x10x16.
+    layer2_weights = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean=mu, stddev=sigma))
+    layer2_bias = tf.Variable(tf.zeros(16))
+    layer2 = conv2d(pool1, layer2_weights, layer2_bias, strides=1)
+
+    # Activation.
+    layer2 = activation(layer2, opt='relu')
+
+    # Pooling. Input = 10x10x16. Output = 5x5x16.
+    pool2 = maxpool2d(layer2, 2, 2)
+
+    # Flatten. Input = 5x5x16. Output = 400.
+    fc0 = flatten(pool2)
+
+    # Layer 3: Fully Connected. Input = 400. Output = 120.
+    fc1_weights = tf.Variable(tf.truncated_normal(shape=(400, 120), mean=mu, stddev=sigma))
+    fc1_bias = tf.Variable(tf.zeros(120))
+    fc1 = tf.matmul(fc0, fc1_weights) + fc1_bias
+
+    # Activation.
+    fc1 = activation(fc1, opt='relu')
+
+    # Layer 4: Fully Connected. Input = 120. Output = 84.
+    fc2_weights = tf.Variable(tf.truncated_normal(shape=(120, 84), mean=mu, stddev=sigma))
+    fc2_bias = tf.Variable(tf.zeros(84))
+    fc2 = tf.matmul(fc1, fc2_weights) + fc2_bias
+
+    # Activation.
+    fc2 = activation(fc2, opt='relu')
+
+    # Layer 5: Fully Connected. Input = 84. Output = 10.
+    fc3_weights = tf.Variable(tf.truncated_normal(shape=(84, 10), mean=mu, stddev=sigma))
+    fc3_bias = tf.Variable(tf.zeros(10))
+    logits = tf.matmul(fc2, fc3_weights) + fc3_bias
+
+    return logits
+
+
+def optimize(eta = 0.001, opt='Adam'):
+    if opt == 'Adam':
+        return tf.train.AdamOptimizer(learning_rate=eta)
+    elif opt == 'Adagrad':
+        return tf.train.AdagradOptimizer(learning_rate=eta)
+    elif opt == 'Momentum':
+        return tf.train.MomentumOptimizer(learning_rate=eta)
+    elif opt == 'AdagradDA':
+        return tf.train.AdagradDAOptimizer(learning_rate=eta)
+    elif opt == 'AdadeltaOptimizer':
+        return tf.train.AdadeltaOptimizer(learning_rate=eta)
+
+
